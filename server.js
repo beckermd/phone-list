22939e38d4e74afe6e955a797d6921cade162c59
Emmanuel Vo <emmanuelvo777@gmail.com>
2016-10-18 15:17:55 -0700

    Able to get a working app. Will refactor with mongoose.

--- server.js
+++ server.js
@@ -1,27 +1,56 @@
 var express = require("express");
 var app = express();
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/test');
-
-
-//var User = mongoose.model("User", {phoneNumber: String});
-
-var userSchema = mongoose.Schema({
-  phoneNumber: String
-});
-
-var User = mongoose.model('User', userSchema);
-
-var user = new User({phoneNumber: "(777) 777-7777"});
-user.save(function(err, data) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-})
+var mongojs = require("mongojs");
+var db = mongojs("phonelist", ["phonelist"]);
+var bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/view"));
+app.use(bodyParser.json());
+
+app.get('/phonelist', function(req, res) {
+  console.log("I received a GET request");
+
+  db.phonelist.find(function(err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post("/phonelist", function(req, res) {
+  console.log(req.body);
+  db.phonelist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete("/phonelist/:id", function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.phonelist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+    res.json(doc);
+  });
+})
+
+app.get('/phonelist/:id', function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.phonelist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/phonelist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.phonelist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
 
 app.listen(5000);
-console.log("Listening on port 5000....");
+console.log("Server running on port 5000");
+
--- view/controller/controller.js
+++ view/controller/controller.js
@@ -1,37 +1,44 @@
-var myApp = angular.module("myApp", []);
-myApp.controller("AppCtrl", ["$scope", function($scope, $http) {
-  console.log("Hello from controller");
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log("Hello World from controller");
+
 
 var refresh = function() {
-  $http.get("/phonelist").then(function(response) {
-    $scope.phonelist = response.data;
-    $scope.contact = "";
-  });
-};
+  $http.get('/phonelist').then(function(response){
+          console.log("I got the data that I requested");
+          $scope.phonelist = response.data;
+          $scope.contact = "";
+      });﻿
+}
+
+refresh();
 
 
-$scope.addNumber = function() {
+$scope.addContact = function() {
   console.log($scope.contact);
-  $http.post("/phonelist", $scope.contact).success(function(response) {
-    console.log(response);
-    refresh();
-  })
-};
+    $http.post("/phonelist", $scope.contact).success(function(response) {
+      console.log(response);
+      refresh();
+    });
+  };
 
 $scope.remove = function(id) {
-  console.log(id)
-  $http.delete("/phonelist/" + id).success(function(response) {
+  console.log(id);
+  $http.delete('/phonelist/' + id).success(function(response) {
     refresh();
   });
 };
 
 $scope.edit = function(id) {
+  console.log(id);
   $http.get("/phonelist/" + id).success(function(response) {
     $scope.contact = response;
   });
 };
 
+
 $scope.update = function() {
+  console.log($scope.contact._id);
   $http.put('/phonelist/' + $scope.contact._id, $scope.contact).success(function(response) {
     refresh();
   });
@@ -42,3 +49,6 @@ $scope.deselect = function() {
 }
 
 }]);
+
+
+
