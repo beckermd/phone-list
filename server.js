a5064a2e587820b6874e70e5917ac4ef8b5b7adc
Emmanuel Vo <emmanuelvo777@gmail.com>
2016-10-18 15:37:47 -0700

    Added a background picture and fix title name.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser");
 app.use(express.static(__dirname + "/view"));
 app.use(bodyParser.json());
 
+
 app.get('/phonelist', function(req, res) {
   console.log("I received a GET request");
 
@@ -14,6 +15,7 @@ app.get('/phonelist', function(req, res) {
     console.log(docs);
     res.json(docs);
   });
+
 });
 
 app.post("/phonelist", function(req, res) {
@@ -50,6 +52,39 @@ app.put('/phonelist/:id', function (req, res) {
     }
   );
 });
+
+/*
+
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test');
+
+
+//var User = mongoose.model("User", {phoneNumber: String});
+
+var userSchema = mongoose.Schema({
+  phoneNumber: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+var user = new User({phoneNumber: "(777) 777-7777"});
+user.save(function(err, data) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+})
+
+app.use(express.static(__dirname + "/view"));
+
+app.listen(5000);
+console.log("Listening on port 5000....");
+
+
+*/
 
 app.listen(5000);
 console.log("Server running on port 5000");
